8393ef127e1aedde83730690df0b90197dfd5181
evaristofm <evaristofm@gmail.com>
2020-10-24 01:35:40 -0300

    adding filter in template customer

---
+++ accounts/filters.py
@@ -0,0 +1,13 @@
+from django_filters import FilterSet, DateFilter
+
+from .models import Order
+
+
+class OrderFilter(FilterSet):
+    start_date = DateFilter(field_name='date_created', lookup_expr='gte')
+    end_date = DateFilter(field_name='date_created', lookup_expr='lte')
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+        exclude = ['customer', 'date_created']
